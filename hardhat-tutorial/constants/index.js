21d16ba14d52062530b8b3d99f8b0d24dbdca3b4
gisantiago <gisantiago@live.com>
2022-03-31 09:05:03 -0400

    solidity contract and deployment

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0x25B276fA1BB9d9aFB3B936c0ea8f3a2B0378b422";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
